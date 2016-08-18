9bad14b8e3078577d6f2deb6aa8a44c3ff7ad5c3
Martin Peeters <martin.peeters@affinitic.be>
2016-08-18 17:24:04 +0200

    Add indexes

---
+++ src/cpskin/citizen/indexers.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+cpskin.citizen
+--------------
+
+Created by mpeeters
+:license: GPL, see LICENCE.txt for more details.
+"""
+
+from plone.indexer import indexer
+from plone.app.stagingbehavior.utils import get_working_copy
+
+from cpskin.citizen import utils
+from cpskin.citizen.behavior import ICitizenAccess
+
+
+@indexer(ICitizenAccess)
+def citizens_indexer(obj):
+    if obj != get_working_copy(obj):
+        return obj.citizens
+
+
+@indexer(ICitizenAccess)
+def validation_required_indexer(obj):
+    if obj == get_working_copy(obj):
+        annotations = utils.get_annotations(obj)
+        return annotations.get('validation_required', False)
+    return False
+
+
+@indexer(ICitizenAccess)
+def citizen_claim_indexer(obj):
+    if obj != get_working_copy(obj):
+        annotations = utils.get_annotations(obj)
+        return annotations.get('claim', [])
