32f2caf33c83f0418c67e0c40d8d15b8c4e24e7f
Benoît Suttor <bsuttor@imio.be>
2016-09-15 17:09:16 +0200

    subscribes to Products.PlonePAS.events.UserLoggedInEvent

--- src/cpskin/citizen/browser/citizen_first_login.py
+++ src/cpskin/citizen/browser/citizen_first_login.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-from geopy.geocoders.osm import Nominatim
-from geopy.geocoders.googlev3 import GoogleV3
-from geopy.exc import GeocoderTimedOut
-
+from cpskin.citizen.utils import is_citizen
+# from geopy.geocoders.osm import Nominatim
+# from geopy.geocoders.googlev3 import GoogleV3
+# from geopy.exc import GeocoderTimedOut
+from plone import api
 import geocoder
 import logging
 logger = logging.getLogger('cpskin.citizen get lat lon')
@@ -10,37 +11,49 @@ logger = logging.getLogger('cpskin.citizen get lat lon')
 
 def create_lat_lon(event):
     user = event.object
-    loc = get_lat_lon_from_address(
-        user.street, user.number, user.zip_code, user.location)
+    if not is_citizen(user):
+        return
+    else:
+        geocode = get_lat_lon_from_address(
+            user.getProperty('street'),
+            user.getProperty('number'),
+            user.getProperty('zip_code'),
+            user.getProperty('location')
+        )
+        if geocode:
+            username = user.getUserName()
+            member = api.user.get(username=username)
+            member.setMemberProperties(mapping={'latitude': geocode.lat})
+            member.setMemberProperties(mapping={'longitude': geocode.lng})
 
 
 def get_lat_lon_from_address(street, number, zip_code, city):
-    location = None
+    # location = None
     if street is not None and city is not None:
         address = "{} {} {} {}".format(
             number,
-            street.encode('utf8'),
+            street,
             zip_code,
             city.encode('utf8'))
-        g = geocoder.google(address)
-        if g:
-            return g.latlng
-        location = get_address(Nominatim(), address, 'OSM')
-        if not location:
-            location = get_address(GoogleV3(), address, 'Google')
-        if not location:
-            return
-    return location
+        geocode = geocoder.google(address)
+        if geocode:
+            return geocode
+    #     location = get_address(Nominatim(), address, 'OSM')
+    #     if not location:
+    #         location = get_address(GoogleV3(), address, 'Google')
+    #     if not location:
+    #         return
+    # return location
 
 
-def get_address(geolocator, address, geolocator_name):
-    try:
-        location = geolocator.geocode(address)
-    except GeocoderTimedOut:
-        logger.error("{} timeout".format(geolocator_name))
-        return False
-    if not location:
-        logger.error("{} didn't know this address '{}'".format(
-            geolocator_name, address))
-        return False
-    return {'lat': location.latitude, 'lon': location.longitude}
+# def get_address(geolocator, address, geolocator_name):
+#     try:
+#         location = geolocator.geocode(address)
+#     except GeocoderTimedOut:
+#         logger.error("{} timeout".format(geolocator_name))
+#         return False
+#     if not location:
+#         logger.error("{} didn't know this address '{}'".format(
+#             geolocator_name, address))
+#         return False
+#     return {'lat': location.latitude, 'lon': location.longitude}
