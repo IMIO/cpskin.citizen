433d108587b9625c448843f787618914a8669415
Christophe Boulanger <christophe.boulanger@imio.be>
2018-11-30 09:15:42 +0100

    Remove unused utils import

--- src/cpskin/citizen/subscriber.py
+++ src/cpskin/citizen/subscriber.py
@@ -3,8 +3,6 @@
 from plone import api
 from plone.app.stagingbehavior.utils import get_working_copy
 
-from cpskin.citizen import utils
-
 
 def citizen_content_removed(obj, event):
     draft = get_working_copy(obj)
